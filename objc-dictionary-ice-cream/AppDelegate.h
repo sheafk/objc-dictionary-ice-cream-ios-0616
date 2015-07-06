44a2992910398a301f86a8f333bc45a2978de7b8
markedwardmurray <markedwardmurray@gmail.com>
2015-07-06 15:19:54 -0400

    created xcodeproj

---
+++ objc-dictionary-ice-cream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-dictionary-ice-cream
+//
+//  Created by Mark Murray on 7/6/15.
+//  Copyright (c) 2015 Mark Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-dictionary-ice-cream/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc-dictionary-ice-cream
+//
+//  Created by Mark Murray on 7/6/15.
+//  Copyright (c) 2015 Mark Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
