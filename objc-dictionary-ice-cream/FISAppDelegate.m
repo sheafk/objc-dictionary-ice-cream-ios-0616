d63d65e179090001f71dadfc35aa1ae3f2dc9f59
markedwardmurray <markedwardmurray@gmail.com>
2015-09-21 11:07:06 -0400

    remove comment headers

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -1,16 +1,6 @@
-//
 //  FISAppDelegate.m
-//  objc-dictionary-ice-cream
-//
-//  Created by Mark Murray on 7/6/15.
-//  Copyright (c) 2015 Mark Murray. All rights reserved.
-//
 
 #import "FISAppDelegate.h"
-
-@interface FISAppDelegate ()
-
-@end
 
 @implementation FISAppDelegate
 
