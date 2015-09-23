d4524784766c313c90a81ba39762b7dffe9fb961
markedwardmurray <markedwardmurray@gmail.com>
2015-09-23 15:23:56 -0400

    pods: update specta 1.0.4 expecta 1.0.3

--- Pods/Target Support Files/Pods-objc-dictionary-ice-creamTests-Expecta/Pods-objc-dictionary-ice-creamTests-Expecta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_objc_dictionary_ice_creamTests_Expecta : NSObject
-@end
-@implementation PodsDummy_Pods_objc_dictionary_ice_creamTests_Expecta
-@end
--- Pods/Target Support Files/Pods-objc-dictionary-ice-creamTests-Specta/Pods-objc-dictionary-ice-creamTests-Specta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_objc_dictionary_ice_creamTests_Specta : NSObject
-@end
-@implementation PodsDummy_Pods_objc_dictionary_ice_creamTests_Specta
-@end
